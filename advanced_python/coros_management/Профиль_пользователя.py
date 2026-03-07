6ada3b8a11d68b307b2800adb10510f47db5ccb3
KumundzhievMaxim <37558223+MaxKumundzhiev@users.noreply.github.com>
2026-03-07 17:33:51 +0400

    main, coro user profile;

---
+++ advanced_python/coros_management/Профиль_пользователя.py
@@ -0,0 +1,49 @@
+"""
+Есть async-функция get_user(user_id), которая читает пользователя из БД.
+Нужно написать handle_request(user_id), которая:
+    - получает пользователя
+    - если он не найден — возвращает "not found"
+    - если найден — возвращает его email
+"""
+
+from typing import Optional, List, Dict
+from dataclasses import dataclass
+from abc import ABC, abstractmethod
+
+
+@dataclass
+class User:
+    user_id: int
+    email: str
+
+
+# interfaces
+class UserRepository(ABC):
+    async def get_user(self, user_id: int) -> Optional[User]:
+        raise NotImplemented
+
+
+class UserService(ABC):
+    async def handle_request(self, user_id: int) -> str:
+        raise NotImplemented
+
+
+# implementation
+class SimpleUserRepository(UserRepository):
+    def __init__(self, users: List[User] | None = None) -> None:
+        self._users: List[User] = users or []
+        self._users_by_id: Dict[int, User] = {u.user_id: u for u in self._users}
+
+    async def get_user(self, user_id: int) -> Optional[User]:
+        return self._users_by_id.get(user_id)
+
+
+class UserServiceImpl(UserService):
+    def __init__(self, repo: UserRepository) -> None:
+        self._repo = repo
+
+    async def handle_request(self, user_id: int) -> str:
+        user = await self._repo.get_user(user_id)
+        if not user:
+            return "Not found"
+        return user.email
